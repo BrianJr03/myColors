cc550f0d2969c765cad085d56ad355d1de0294fc
Brian Walker Jr <thaballa79@gmail.com>
2022-09-07 22:03:50 -0500

    adds room db testing

---
+++ app/src/androidTest/java/jr/brian/myapplicatid/db/StoreDaoTest.kt
@@ -0,0 +1,96 @@
+package jr.brian.myapplicatid.db
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth
+import jr.brian.myapplication.model.local.AppDatabase
+import jr.brian.myapplication.model.local.FavColorsDao
+import jr.brian.myapplication.model.remote.MyColor
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class FavColorsDaoTest {
+
+    private lateinit var appDB: AppDatabase
+    private lateinit var dao: FavColorsDao
+
+    private val color1 = MyColor(hex = "#E48762", hsl = "", rgb = "")
+    private val color2 = MyColor(hex = "#CEF8B0", hsl = "", rgb = "")
+    private val color3 = MyColor(hex = "#77ECFE", hsl = "", rgb = "")
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Before
+    fun init() {
+        appDB = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            AppDatabase::class.java
+        ).build()
+        dao = appDB.dao()
+    }
+
+    @After
+    fun close() {
+        appDB.close()
+    }
+
+    @Test
+    fun testInsertFavColors() {
+        runTest(StandardTestDispatcher()) {
+            dao.apply {
+                insertFavColor(color1)
+                insertFavColor(color2)
+                insertFavColor(color3)
+            }
+
+            val favColors = dao.getFavColors()
+
+            Truth.assertThat(favColors).containsExactly(color1, color2, color3)
+        }
+    }
+
+    @Test
+    fun testRemoveFavColor() {
+        runTest(StandardTestDispatcher()) {
+            dao.apply {
+                insertFavColor(color1)
+                insertFavColor(color2)
+                insertFavColor(color3)
+                removeFavColor(color1)
+            }
+
+            val favColors = dao.getFavColors()
+
+            Truth.assertThat(favColors).containsExactly(color2, color3)
+        }
+    }
+
+    @Test
+    fun testRemoveAllFavColors() {
+        runTest(StandardTestDispatcher()) {
+            dao.apply {
+                insertFavColor(color1)
+                insertFavColor(color2)
+                insertFavColor(color3)
+                removeAllFavColors()
+            }
+
+            val favColors = dao.getFavColors()
+
+            Truth.assertThat(favColors).isEmpty()
+        }
+    }
+}
--- app/src/main/java/jr/brian/myapplication/model/local/FavColorsDao.kt
+++ app/src/main/java/jr/brian/myapplication/model/local/FavColorsDao.kt
@@ -11,15 +11,17 @@ interface FavColorsDao {
     @Query("SELECT * FROM color_table")
     fun getFavColors(): List<MyColor>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun passStartUp(sui: StartUpIntro)
-
-    @Query("SELECT * FROM start_up_table")
-    fun getStartUpPass(): List<StartUpIntro>
-
     @Delete
     fun removeFavColor(color: MyColor)
 
     @Query("DELETE FROM color_table")
     fun removeAllFavColors()
+
+    // TODO - Save in DataStore
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun passStartUp(sui: StartUpIntro)
+
+    // TODO - Save in DataStore
+    @Query("SELECT * FROM start_up_table")
+    fun getStartUpPass(): List<StartUpIntro>
 }
