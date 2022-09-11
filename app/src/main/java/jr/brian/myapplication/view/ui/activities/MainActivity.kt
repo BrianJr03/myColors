56410e8527e2dd02675228a641d438999b6c3d39
Brian Walker Jr <thaballa79@gmail.com>
2022-09-10 23:18:42 -0500

    add nav animation

--- app/src/main/java/jr/brian/myapplication/view/ui/activities/MainActivity.kt
+++ app/src/main/java/jr/brian/myapplication/view/ui/activities/MainActivity.kt
@@ -3,6 +3,9 @@ package jr.brian.myapplication.view.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,9 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 import jr.brian.myapplication.data.model.local.FavColorsDao
 import jr.brian.myapplication.util.MyDataStore
@@ -21,6 +24,7 @@ import jr.brian.myapplication.view.ui.pages.HomePage
 import jr.brian.myapplication.view.ui.pages.StartUpPage
 import jr.brian.myapplication.view.ui.theme.ComposeTheme
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -45,43 +49,71 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun AppUI(dao: FavColorsDao, dataStore: MyDataStore) {
-    val navController = rememberNavController()
+    val navController = rememberAnimatedNavController()
     val didPassStartUp = dataStore.getStartUpPassStatus.collectAsState(initial = false)
     val dest = if (didPassStartUp.value == true) "home_page" else "start_up_page"
-    NavHost(navController = navController, startDestination = dest, builder = {
-        composable(
-            "home_page",
-            content = {
-                HomePage(
-                    dao = dao,
-                    onNavigateToStartUp = {
-                        navController.navigate("start_up_page") {
-                            popUpTo(navController.graph.startDestinationId)
-                            launchSingleTop = true
+    AnimatedNavHost(
+        navController = navController,
+        startDestination = dest,
+        enterTransition = {
+            slideIntoContainer(
+                AnimatedContentScope.SlideDirection.Left,
+                animationSpec = tween(700)
+            )
+        },
+        exitTransition = {
+            slideOutOfContainer(
+                AnimatedContentScope.SlideDirection.Left,
+                animationSpec = tween(700)
+            )
+        },
+        popEnterTransition = {
+            slideIntoContainer(
+                AnimatedContentScope.SlideDirection.Right,
+                animationSpec = tween(700)
+            )
+        },
+        popExitTransition = {
+            slideOutOfContainer(
+                AnimatedContentScope.SlideDirection.Right,
+                animationSpec = tween(700)
+            )
+        },
+        builder = {
+            composable(
+                "home_page",
+                content = {
+                    HomePage(
+                        dao = dao,
+                        onNavigateToStartUp = {
+                            navController.navigate("start_up_page") {
+                                popUpTo(navController.graph.startDestinationId)
+                                launchSingleTop = true
+                            }
+                        },
+                        onNavigateToFavorites = {
+                            navController.navigate("fav_color_page") {
+                                popUpTo(navController.graph.startDestinationId)
+                                launchSingleTop = true
+                            }
                         }
-                    },
-                    onNavigateToFavorites = {
-                        navController.navigate("fav_color_page") {
+                    )
+                })
+            composable(
+                "fav_color_page",
+                content = { FavColorPage(dao = dao) })
+            composable(
+                "start_up_page",
+                content = {
+                    StartUpPage {
+                        navController.navigate("home_page") {
                             popUpTo(navController.graph.startDestinationId)
                             launchSingleTop = true
                         }
                     }
-                )
-            })
-        composable(
-            "fav_color_page",
-            content = { FavColorPage(dao = dao) })
-        composable(
-            "start_up_page",
-            content = {
-                StartUpPage {
-                    navController.navigate("home_page") {
-                        popUpTo(navController.graph.startDestinationId)
-                        launchSingleTop = true
-                    }
-                }
-            })
-    })
+                })
+        })
 }
